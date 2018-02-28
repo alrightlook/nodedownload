8143da9ac3116d483e8b5ca889794c9898ea1a89
root <root@jerry.(none)>
2018-02-28 00:42:18 -0500

    add list file functionality

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require('fs');
+var path = require('path');
+
+var walk = function(dir, done) {
+  var result = [];
+  fs.readdir(dir, function(err, list) {
+  if (err) return done(err);
+  var pending = list.length;
+  if(!pending) return done(null, result);
+  list.forEach(function(file) {
+    file = path.resolve(dir, file);
+    fs.stat(file, function(err, stat) {
+      if(stat && stat.isDirectory()) {
+        walk(file, function(err, res) {
+          result = result.concat(res);
+          if (!--pending) done(null, result);
+        });
+      }
+      else {
+        result.push(file);
+        if(!--pending) done(null, result);
+      }
+    });
+  });
+  });
+};
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -8,5 +34,18 @@ router.get('/', function(req, res, next) {
 
 router.get('/getfile', function(req, res, next) {
   res.download(req.query.url);
+});
+
+router.get('/walk', function(req, res, next) {
+  walk(req.query.url, function(err, result)
+  {
+    if(err) res.statu(404).send("error!!!");
+//    res.status(200).send(result);
+  res.render('walk', {title: 'NoTitle',items: result});
+  });
+});
+
+router.post('/sacheckout', function(req, res, next) {
+  res.status(200).send({sign : 'jerrysa', msg : '' });
 });
 module.exports = router;
